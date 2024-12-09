65d69ea67eaecc052a80bdc5505f150504d69713
Konrad Tercjak <ktercjak0@gmail.com>
2024-12-09 12:40:06 +0100

    moved\ library to  crypto_api dir

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='crypto_api',
+    version='0.1.0',
+    author='Konrad Tercjak',
+    author_email='ktercjak0@gmail.com',
+    description='CryptoAPI is a powerful Python library designed to simplify interaction with various cryptocurrency APIs. (Binance REST and WebSockets)',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/tercjak/CryptoAPI',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+    install_requires=[
+        'numpy>=1.21.0',
+        'requests>=2.25.1',
+        'websocket_client>=1.8.0',
+        'pydantic>=2.10.3'
+    ],
+)
